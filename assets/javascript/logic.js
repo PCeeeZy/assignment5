b38f39e52d228a4a044208acc3f8dc6a8bf9d4ea
Paul <paul@cwik.us>
2018-11-02 14:58:38 -0500

    2nd commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,34 @@
+document.ready(function() {
+    // TRIVIA GAME LOGIC
+
+    // questions ARRAY
+
+    // trueAnswers array ---this is where the correct answers will be saved
+
+    // answers# array for the answers options to be displayed for each question.
+        // answers0 will correlate to questions[0].
+        // answers4 will correlate to questions[4].
+
+    // GLOBAL VARIABLES
+        // timer -- starts at 15 seconds
+        // questionCount -- starts at 0
+
+    // timer function
+        // display a countown of time
+        // if time = 0 then run nextQuestion function
+    
+    // nextQuestion function
+        // push chosen answer or no answer to userAnswers array
+        // hide current question
+        // clear chosen answer displayed
+        // increment questionCount
+        // reset timer
+        // show question = questionCount
+
+    // onclickevent
+        // when finalize answer button is clicked
+            // run nextQuestion
+
+    
+
+})
