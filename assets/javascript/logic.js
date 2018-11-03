b079c79673770334e2d58d929d81c4a0d159d005
Paul Cwik <paul@cwik.us>
2018-11-03 11:38:43 -0500

    3rd commit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,12 +2,19 @@ document.ready(function() {
     // TRIVIA GAME LOGIC
 
     // questions ARRAY
+    var questions = ["In the Harry Potter series, what is the name of Harry's pet owl?", "What school does Harry Potter attend?", "Who famously said, 'Harry-- yer a wizard'?", "In the films, which character does Emma Watson play?", "Which of the four houses of Hogwarts values hard work, dedication, patience, loyalty, and fair play?"];
 
     // trueAnswers array ---this is where the correct answers will be saved
+    var trueAnswers = ["Hedwig", "Hogwarts", "Hagrid", "Hermione", "Hufflepuff"];
 
     // answers# array for the answers options to be displayed for each question.
+    var answers0 = ["Owlbert Einstein", "Owlbus Dumbledore", "Hedwig", "Hoo-Dini"];
         // answers0 will correlate to questions[0].
         // answers4 will correlate to questions[4].
+    var answers1 = ["Durmstrang", "Uagadou", "Beauxbatons", "Hogwarts"];
+    var answers2 = ["Hagrid", "Hester", "Holten", "Hank Hill"];
+    var answers3 = ["Hermione", "Harriet", "Helen", "Crooked Hillary"];
+    var answers4 = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
     // GLOBAL VARIABLES
         // timer -- starts at 15 seconds
